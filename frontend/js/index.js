bd897ad249031a117e65902dcb1703f4ea098646
guilherm5 <estudosrodriguesti@gmail.com>
2024-06-15 14:33:54 -0300

    adicionando overlay de carregamento de download e corrigindo erro 403 do console

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,27 +1,45 @@
 async function download_midia() {
-    // Tratar overlay de carragamento
+    let acaoVideo = ""; 
+    const enderecoAtual = window.location.href;
+    if (enderecoAtual === 'http://127.0.0.1:5500/frontend/index.html?') {
+        acaoVideo = 'download_video';
+    } else if (enderecoAtual === 'http://127.0.0.1:5500/frontend/music.html?') {
+        acaoVideo = 'download_audio';
+    } else {
+        console.log('Nenhuma condição correspondeu à URL atual:', enderecoAtual);
+    }
+
+    // Mostra overlay de carregamento
     document.getElementById("loading-overlay").style.display = "block";
 
     const linkVideo = document.getElementById("link-youtube").value;
     const bodyVideo = JSON.stringify({
         "url_video": linkVideo,
-        "acao_midia": "download_video"
+        "acao_midia": acaoVideo
     });
 
     const headersVideo = new Headers();
     headersVideo.append("Content-Type", "application/json");
-  
-    try{
-        const response = await fetch("http://127.0.0.1:8000/download_midia_youtube",{
+    
+    try {
+        const response = await fetch("http://127.0.0.1:8000/download_midia_youtube", {
             method: "POST",
             body: bodyVideo,
             headers: headersVideo
-        })
-    }catch(error){
-        console.log("Erro na requisição fetch: ", error.message)
-    }finally{
-        /*tratar finally*/ 
-    }
+        });
+        const result = await response.text();
+        console.log("resultado", result);
         
-    
+    } catch (error) {
+        console.error("erro fetch", error);
+    } finally {
+        document.getElementById("loading-overlay").style.display = "none";
+    }
 }
+const btnDownload = document.getElementById('btn-download');
+// Adiciona um event listener para o evento de clique
+btnDownload.addEventListener('click', function(event) {
+    // Previne o comportamento padrão do botão (no caso de estar dentro de um formulário)
+    event.preventDefault();
+    download_midia();
+});
